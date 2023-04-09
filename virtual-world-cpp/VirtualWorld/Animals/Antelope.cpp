7ce1ce135dbbffcf5ffcd4e108c2331ee0398293
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2023-04-09 21:53:05 +0200

    small change

--- virtual-world-cpp/VirtualWorld/Animals/Antelope.cpp
+++ virtual-world-cpp/VirtualWorld/Animals/Antelope.cpp
@@ -36,18 +36,7 @@ Organism *Antelope::Clone(int x, int y, void *world) {
 }
 
 void Antelope::Action() {
-    World* world = (World*)this->world;
-    int x = rand() % 5 - 2;
-    int y = rand() % 5 - 2;
-    if (x == 0 && y == 0) return;
-    if(x + this->x < 0 || x + this->x >= world->GetWidth() || y + this->y < 0 || y + this->y >= world->GetHeight())
-        return;
-    Organism* other = world->GetOrganism(x + this->x, y + this->y);
-    if(other == this)
-        return;
-    if (other != nullptr) {
-        this->Collision(other);
-    } else {
-        world->MoveOrganism(this, x + this->x, y + this->y);
-    }
+    for(int i = 0; i < 2; i++)
+        if(IsAlive())
+            Animal::Action();
 }
